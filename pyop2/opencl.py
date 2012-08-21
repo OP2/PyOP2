55faa2070dac38697900a41712e78d3675aa2d2a
Nicolas Loriant <n.loriant@imperial.ac.uk>
2012-08-21 15:37:10 +0100

    add alias

--- pyop2/opencl.py
+++ pyop2/opencl.py
@@ -117,7 +117,11 @@ class Kernel(op2.Kernel):
 
 class Arg(op2.Arg):
     """OP2 OpenCL argument type."""
-    
+
+    @property
+    def dat(self):
+        return self.data
+
     # Codegen specific
     @property
     def _d_is_staged(self):
