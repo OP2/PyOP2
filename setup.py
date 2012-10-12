efa3fc2e765d732fa1f5b639a23334c1eed205d0
gmarkall <grm08@doc.ic.ac.uk>
2012-10-12 16:33:35 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+import os, sys
+
+try:
+    OP2_DIR = os.environ['OP2_DIR']
+except KeyError:
+    sys.exit("""Error: Could not find OP2 library.
+
+Set the environment variable OP2_DIR to point to the op2 subdirectory
+of your OP2 source tree""")
+
+OP2_INC = OP2_DIR + '/c/include'
+OP2_LIB = OP2_DIR + '/c/lib'
+
+os.environ['CC'] = 'mpicc'
+os.environ['CXX'] = 'mpicxx'
+
+setup(name='pyop2',
+      version='0,1',
+      packages=['pyop2','pyop2_utils'],
+      package_dir={'pyop2':'pyop2','pyop2_utils':'pyop2_utils'},
+      package_data={'pyop2': ['assets/*']},
+      ext_modules=[Extension('pyop2.op_lib_core', ['pyop2/op_lib_core.c'],
+                   pyrex_include_dirs=['pyop2'],
+                   include_dirs=[OP2_INC] + [np.get_include()],
+                   library_dirs=[OP2_LIB],
+                   runtime_library_dirs=[OP2_LIB],
+                   libraries=["op2_seq"])])
