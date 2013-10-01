1c7b8aab3962251b03566ab4654a17ed9cb49d18
Fabio Luporini <f.luporini12@imperial.ac.uk>
2013-10-01 12:34:44 +0100

    Support more types (assign, decl). Code refactoring.

--- pyop2/ir/ast_builder.py
+++ pyop2/ir/ast_builder.py
@@ -5,15 +5,20 @@ from ast_base import *
 
 def add_node(parent, children):
     """Add the given list of children a node"""
-    parent.children = children
+    for child in children:
+        parent.children.append(child)
 
 
 def create_sum(operands, is_leaf):
     """If is_lead is false then wrap the sum with parentheses"""
-    new_sym = Sum()
-    new_sym.children = [Symbol(sym) for sym in operands]
+    op1 = operands[0]
+    op2 = operands[1]
+    new_sym = Sum(Symbol(op1[0], op1[1]), Symbol(op2[0], op2[1]))
     if not is_leaf:
-        par = Parentheses()
-        par.children.append(new_sym)
+        par = Parentheses(new_sym)
         new_sym = par
     return new_sym
+
+
+def assign(operands):
+    Assign(operands[0], operands[1])
--- pyop2/ir/tester.py
+++ pyop2/ir/tester.py
@@ -1,21 +1,31 @@
 # Test the AST data structures and routines
 
 from ast_base import *
-from ast_builder import add_node, create_sum
+from ast_builder import create_sum
 from ast_handler import generate_code
 
 
 def main():
     print "Creating a fake AST..."
-    root = Root()
-    top_sum = Sum()
-    add_node(root, [top_sum])
-    sum1 = create_sum([2, 3], False)
-    sum2 = create_sum([4, 5], False)
-    add_node(top_sum, [sum1, sum2])
+    exp_1 = create_simple_exp()
+    exp_2 = create_array_exp()
+    ass_1 = Assign(Symbol("A", "j"), exp_1)
+    decl_1 = Decl("double", Symbol("B", (3,)), "{3, 3, 3}", ["const"])
+    root = Root([ass_1, exp_2, decl_1])
 
     print "Generate the code for this AST"
     print generate_code(root)
+
+
+def create_simple_exp():
+    sum1 = create_sum(((2, ()), (3, ())), False)
+    sum2 = create_sum(((4, ()), (5, ())), False)
+    return Sum(sum1, sum2)
+
+
+def create_array_exp():
+    return create_sum((("A", ("j", "k")), ("B", ("j", "k"))), True)
+
 
 if __name__ == "__main__":
     main()
