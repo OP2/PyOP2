30724838a406bd3238b9e9339f7d54c61fb26d3f
Doru Rathgeber <a.mcrae12@imperial.ac.uk>
2014-05-30 15:58:36 +0100

    turn off O3 compilation

--- pyop2/compilation.py
+++ pyop2/compilation.py
@@ -394,7 +394,7 @@ class LinuxCompiler(Compiler):
     :kwarg comm: Optional communicator to compile the code on (only
     rank 0 compiles code) (defaults to COMM_WORLD)."""
     def __init__(self, cppargs=[], ldargs=[], cpp=False, comm=None):
-        opt_flags = ['-march=native', '-O3', '-ffast-math']
+        opt_flags = ['-O0']
         if configuration['debug']:
             opt_flags = ['-O0', '-g']
         cc = "mpicc"
