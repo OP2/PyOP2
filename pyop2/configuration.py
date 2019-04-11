f54833c3df5644cfb52b16d1c1671f0226788cc5
Tianjiao Sun <tianjiao.sun14@imperial.ac.uk>
2019-04-11 16:36:14 +0100

    codegen: Implement SIMD vectorisation
    Only works when kernel is a Loopy kernel.

--- pyop2/configuration.py
+++ pyop2/configuration.py
@@ -76,6 +76,7 @@ class Configuration(dict):
     DEFAULTS = {
         "compiler": ("PYOP2_BACKEND_COMPILER", str, "gcc"),
         "simd_width": ("PYOP2_SIMD_WIDTH", int, 4),
+        "alignment": ("PYOP2_ALIGNMENT", int, 64),
         "debug": ("PYOP2_DEBUG", bool, False),
         "cflags": ("PYOP2_CFLAGS", str, ""),
         "ldflags": ("PYOP2_LDFLAGS", str, ""),
