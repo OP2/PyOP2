0cb84c68f44b94c3f80aceeaf5cde1efba71e77e
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-05-02 14:49:37 -0500

    introduces an abstract backend class

---
+++ pyop2/backend.py
@@ -0,0 +1,38 @@
+class _not_implemented:  # noqa
+    """Not Implemented"""
+
+
+class AbstractComputeBackend:
+    Arg = _not_implemented()
+    ParLoop = _not_implemented()
+    Kernel = _not_implemented()
+    Dat = _not_implemented()
+    READ = _not_implemented()
+    WRITE = _not_implemented()
+    RW = _not_implemented()
+    INC = _not_implemented()
+    MIN = _not_implemented()
+    MAX = _not_implemented()
+    Set = _not_implemented()
+    ExtrudedSet = _not_implemented()
+    MixedSet = _not_implemented()
+    Subset = _not_implemented()
+    DataSet = _not_implemented()
+    MixedDataSet = _not_implemented()
+    Map = _not_implemented()
+    MixedMap = _not_implemented()
+    Sparsity = _not_implemented()
+    Halo = _not_implemented()
+    Dat = _not_implemented()
+    MixedDat = _not_implemented()
+    DatView = _not_implemented()
+    Mat = _not_implemented()
+    Global = _not_implemented()
+    GlobalDataSet = _not_implemented()
+
+    def _getattr_(self, key):
+        val = super(AbstractComputeBackend, self)._getattr_(key)
+        if isinstance(val, _not_implemented):
+            raise NotImplementedError("'{}' is not implemented for backend"
+                    " '{}'.".format(val, self.__name__))
+        return val
