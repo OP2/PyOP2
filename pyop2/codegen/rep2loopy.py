e7fc25e68f2637b01595fdcf9115925a88c17977
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-04-19 17:28:31 +0200

    Loopy callables: register the callables rather than functions and remove the lookup functions.

--- pyop2/codegen/rep2loopy.py
+++ pyop2/codegen/rep2loopy.py
@@ -98,12 +98,6 @@ def register_petsc_function(name):
     petsc_functions.add(name)
 
 
-def petsc_function_lookup(target, identifier):
-    if identifier in petsc_functions:
-        return PetscCallable(name=identifier)
-    return None
-
-
 class LACallable(loopy.ScalarCallable, metaclass=abc.ABCMeta):
     """
     The LACallable (Linear algebra callable)
@@ -263,27 +257,6 @@ class PyOP2KernelCallable(loopy.ScalarCallable):
 
         assignee_is_returned = False
         return var(self.name_in_target)(*c_parameters), assignee_is_returned
-
-
-class PyOP2KernelLookup(object):
-
-    def __init__(self, name, code, access):
-        self.name = name
-        self.code = code
-        self.access = access
-
-    def __hash__(self):
-        return hash(self.name + self.code)
-
-    def __eq__(self, other):
-        if isinstance(other, PyOP2KernelLookup):
-            return self.name == other.name and self.code == other.code
-        return False
-
-    def __call__(self, target, identifier):
-        if identifier == self.name:
-            return PyOP2KernelCallable(name=identifier, access=self.access)
-        return None
 
 
 @singledispatch
@@ -579,15 +552,16 @@ def generate(builder, wrapper_name=None):
             code = kernel._code.gencode()
         else:
             code = kernel._code
-        wrapper = loopy.register_function_id_to_in_knl_callable_mapper(
-            wrapper,
-            PyOP2KernelLookup(kernel.name, code, tuple(builder.argument_accesses)))
+        wrapper = loopy.register_callable(
+            wrapper, kernel.name,
+            PyOP2KernelCallable(name=kernel.name, access=tuple(builder.argument_accesses)))
         preamble = preamble + "\n" + code
 
     wrapper = loopy.register_preamble_generators(wrapper, [_PreambleGen(preamble)])
 
     # register petsc functions
-    wrapper = loopy.register_function_id_to_in_knl_callable_mapper(wrapper, petsc_function_lookup)
+    for identifier in petsc_functions:
+        wrapper = loopy.register_callable(wrapper, PetscCallable(name=identifier))
 
     return wrapper
 
