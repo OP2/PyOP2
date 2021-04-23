9b02cf7818bd5bd2ad2618fc858839d7f8479ea4
Lawrence Mitchell <lawrence@wence.uk>
2021-04-23 16:57:37 +0100

    Start adapting pyop2 kernels to shaped dats

--- pyop2/base.py
+++ pyop2/base.py
@@ -1555,16 +1555,20 @@ class Dat(DataCarrier, _EmptyDataMixin):
                 import islpy as isl
                 import pymbolic.primitives as p
 
-                inames = isl.make_zero_and_vars(["i"])
-                domain = (inames[0].le_set(inames["i"])) & (inames["i"].lt_set(inames[0] + self.cdim))
+                dim = self.dim or (1,)
+                index_names = [f"i{i}" for i in range(len(dim))]
+                inames = isl.make_zero_and_vars(index_names)
+                domains = [reduce(operator.and_,
+                                  (((inames[0].le_set(inames[i])) & (inames[i].lt_set(inames[0] + d)))
+                                   for i, d in zip(index_names, dim)))]
                 x = p.Variable("dat")
-                i = p.Variable("i")
-                insn = loopy.Assignment(x.index(i), 0, within_inames=frozenset(["i"]))
-                data = loopy.GlobalArg("dat", dtype=self.dtype, shape=(self.cdim,))
-                knl = loopy.make_function([domain], [insn], [data], name="zero", target=loopy.CTarget(), lang_version=(2018, 2))
+                indices = tuple(p.Variable(n) for n in index_names)
+                insn = loopy.Assignment(x[indices], 0, within_inames=frozenset(index_names))
+                data = loopy.GlobalArg("dat", dtype=self.dtype, shape=dim)
+                knl = loopy.make_function(domains, [insn], [data], name="zero", target=loopy.CTarget(), lang_version=(2018, 2))
 
                 knl = _make_object('Kernel', knl, 'zero')
-                self._zero_kernels[(self.dtype, self.cdim)] = knl
+                self._zero_kernels[(self.dtype, dim)] = knl
             loop = _make_object('ParLoop', knl,
                                 iterset,
                                 self(WRITE))
@@ -1587,15 +1591,20 @@ class Dat(DataCarrier, _EmptyDataMixin):
         if not hasattr(self, '_copy_kernel'):
             import islpy as isl
             import pymbolic.primitives as p
-            inames = isl.make_zero_and_vars(["i"])
-            domain = (inames[0].le_set(inames["i"])) & (inames["i"].lt_set(inames[0] + self.cdim))
+            dim = self.dim or (1,)
+            index_names = [f"i{i}" for i in range(len(dim))]
+            inames = isl.make_zero_and_vars(index_names)
+            domains = [reduce(operator.and_,
+                              (((inames[0].le_set(inames[i])) & (inames[i].lt_set(inames[0] + d)))
+                               for i, d in zip(index_names, dim)))]
+            indices = tuple(p.Variable(n) for n in index_names)
+            insn = loopy.Assignment(x[indices], 0, within_inames=frozenset(index_names))
             _other = p.Variable("other")
             _self = p.Variable("self")
-            i = p.Variable("i")
-            insn = loopy.Assignment(_other.index(i), _self.index(i), within_inames=frozenset(["i"]))
-            data = [loopy.GlobalArg("self", dtype=self.dtype, shape=(self.cdim,)),
-                    loopy.GlobalArg("other", dtype=other.dtype, shape=(other.cdim,))]
-            knl = loopy.make_function([domain], [insn], data, name="copy", target=loopy.CTarget(), lang_version=(2018, 2))
+            insn = loopy.Assignment(_other[indices], _self[indices], within_inames=frozenset(index_names))
+            data = [loopy.GlobalArg("self", dtype=self.dtype, shape=dim),
+                    loopy.GlobalArg("other", dtype=other.dtype, shape=dim)]
+            knl = loopy.make_function(domains, [insn], data, name="copy", target=loopy.CTarget(), lang_version=(2018, 2))
 
             self._copy_kernel = _make_object('Kernel', knl, 'copy')
         return _make_object('ParLoop', self._copy_kernel,
