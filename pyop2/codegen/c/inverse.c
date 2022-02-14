ae0fa256531fd5132c61530ac4450743c483c956
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-02-14 15:45:36 +0100

    No need for class id and make event ids static.

--- pyop2/codegen/c/inverse.c
+++ pyop2/codegen/c/inverse.c
@@ -1,6 +1,5 @@
 #include <petscsys.h>
 #include <petscblaslapack.h>
-#include <petsclog.h>
 
 #ifndef PYOP2_WORK_ARRAYS
 #define PYOP2_WORK_ARRAYS
@@ -9,33 +8,34 @@ static PetscBLASInt ipiv_buffer[BUF_SIZE];
 static PetscScalar work_buffer[BUF_SIZE*BUF_SIZE];
 #endif
 
+#ifndef PYOP2_INV_LOG_EVENTS
+#define PYOP2_INV_LOG_EVENTS
+static PetscLogEvent USER_EVENT_inv_memcpy;
+static PetscLogEvent USER_EVENT_inv_getrf;
+static PetscLogEvent USER_EVENT_inv_getri;
+#endif
+
 static void inverse(PetscScalar* __restrict__ Aout, const PetscScalar* __restrict__ A, PetscBLASInt N)
 {
-    PetscLogEvent  USER_EVENT_memcpy;
-    PetscLogEvent  USER_EVENT_getrf;
-    PetscLogEvent  USER_EVENT_getri;
-    PetscClassId   classid;
+    PetscLogEventRegister("PyOP2InverseCallable_memcpy",PETSC_OBJECT_CLASSID,&USER_EVENT_inv_memcpy);
+    PetscLogEventRegister("PyOP2InverseCallable_getrf",PETSC_OBJECT_CLASSID,&USER_EVENT_inv_getrf);
+    PetscLogEventRegister("PyOP2InverseCallable_getri",PETSC_OBJECT_CLASSID,&USER_EVENT_inv_getri);
 
-    PetscClassIdRegister("PyOP2InverseCallable",&classid);
-    PetscLogEventRegister("PyOP2InverseCallable_memcpy",classid,&USER_EVENT_memcpy);
-    PetscLogEventRegister("PyOP2InverseCallable_getrf",classid,&USER_EVENT_getrf);
-    PetscLogEventRegister("PyOP2InverseCallable_getri",classid,&USER_EVENT_getri);
-
-    PetscLogEventBegin(USER_EVENT_memcpy,0,0,0,0);
+    PetscLogEventBegin(USER_EVENT_inv_memcpy,0,0,0,0);
     PetscBLASInt info;
     PetscBLASInt *ipiv = N <= BUF_SIZE ? ipiv_buffer : malloc(N*sizeof(*ipiv));
     PetscScalar *Awork = N <= BUF_SIZE ? work_buffer : malloc(N*N*sizeof(*Awork));
     memcpy(Aout, A, N*N*sizeof(PetscScalar));
-    PetscLogEventEnd(USER_EVENT_memcpy,0,0,0,0);
+    PetscLogEventEnd(USER_EVENT_inv_memcpy,0,0,0,0);
 
-    PetscLogEventBegin(USER_EVENT_getrf,0,0,0,0);
+    PetscLogEventBegin(USER_EVENT_inv_getrf,0,0,0,0);
     LAPACKgetrf_(&N, &N, Aout, &N, ipiv, &info);
-    PetscLogEventEnd(USER_EVENT_getrf,0,0,0,0);
+    PetscLogEventEnd(USER_EVENT_inv_getrf,0,0,0,0);
 
     if(info == 0){
-        PetscLogEventBegin(USER_EVENT_getri,0,0,0,0);
+        PetscLogEventBegin(USER_EVENT_inv_getri,0,0,0,0);
         LAPACKgetri_(&N, Aout, &N, ipiv, Awork, &N, &info);
-        PetscLogEventEnd(USER_EVENT_getri,0,0,0,0);
+        PetscLogEventEnd(USER_EVENT_inv_getri,0,0,0,0);
     }
 
     if(info != 0){
--- pyop2/codegen/c/solve.c
+++ pyop2/codegen/c/solve.c
@@ -1,6 +1,5 @@
 #include <petscsys.h>
 #include <petscblaslapack.h>
-#include <petsclog.h>
 
 #ifndef PYOP2_WORK_ARRAYS
 #define PYOP2_WORK_ARRAYS
@@ -9,36 +8,37 @@ static PetscBLASInt ipiv_buffer[BUF_SIZE];
 static PetscScalar work_buffer[BUF_SIZE*BUF_SIZE];
 #endif
 
+#ifndef PYOP2_SOLVE_LOG_EVENTS
+#define PYOP2_SOLVE_LOG_EVENTS
+static PetscLogEvent USER_EVENT_solve_memcpy;
+static PetscLogEvent USER_EVENT_solve_getrf;
+static PetscLogEvent USER_EVENT_solve_getrs;
+#endif
+
 static void solve(PetscScalar* __restrict__ out, const PetscScalar* __restrict__ A, const PetscScalar* __restrict__ B, PetscBLASInt N)
 {
-    PetscLogEvent  USER_EVENT_memcpy;
-    PetscLogEvent  USER_EVENT_getrf;
-    PetscLogEvent  USER_EVENT_getrs;
-    PetscClassId   classid;
+    PetscLogEventRegister("PyOP2SolveCallable_memcpy",PETSC_OBJECT_CLASSID,&USER_EVENT_solve_memcpy);
+    PetscLogEventRegister("PyOP2SolveCallable_getrf",PETSC_OBJECT_CLASSID,&USER_EVENT_solve_getrf);
+    PetscLogEventRegister("PyOP2SolveCallable_getrs",PETSC_OBJECT_CLASSID,&USER_EVENT_solve_getrs);
 
-    PetscClassIdRegister("PyOP2SolveCallable",&classid);
-    PetscLogEventRegister("PyOP2SolveCallable_memcpy",classid,&USER_EVENT_memcpy);
-    PetscLogEventRegister("PyOP2SolveCallable_getrf",classid,&USER_EVENT_getrf);
-    PetscLogEventRegister("PyOP2SolveCallable_gerts",classid,&USER_EVENT_getrs);
-
-    PetscLogEventBegin(USER_EVENT_memcpy,0,0,0,0);
+    PetscLogEventBegin(USER_EVENT_solve_memcpy,0,0,0,0);
     PetscBLASInt info;
     PetscBLASInt *ipiv = N <= BUF_SIZE ? ipiv_buffer : malloc(N*sizeof(*ipiv));
     memcpy(out,B,N*sizeof(PetscScalar));
     PetscScalar *Awork = N <= BUF_SIZE ? work_buffer : malloc(N*N*sizeof(*Awork));
     memcpy(Awork,A,N*N*sizeof(PetscScalar));
-    PetscLogEventEnd(USER_EVENT_memcpy,0,0,0,0);
+    PetscLogEventEnd(USER_EVENT_solve_memcpy,0,0,0,0);
 
     PetscBLASInt NRHS = 1;
     const char T = 'T';
-    PetscLogEventBegin(USER_EVENT_getrf,0,0,0,0);
+    PetscLogEventBegin(USER_EVENT_solve_getrf,0,0,0,0);
     LAPACKgetrf_(&N, &N, Awork, &N, ipiv, &info);
-    PetscLogEventEnd(USER_EVENT_getrf,0,0,0,0);
+    PetscLogEventEnd(USER_EVENT_solve_getrf,0,0,0,0);
 
     if(info == 0){
-        PetscLogEventBegin(USER_EVENT_getrs,0,0,0,0);
+        PetscLogEventBegin(USER_EVENT_solve_getrs,0,0,0,0);
         LAPACKgetrs_(&T, &N, &NRHS, Awork, &N, ipiv, out, &N, &info);
-        PetscLogEventEnd(USER_EVENT_getrs,0,0,0,0);
+        PetscLogEventEnd(USER_EVENT_solve_getrs,0,0,0,0);
     }
 
     if(info != 0){
